c140969265612334ca86b6848ea0a903c9523e3b
yuta-miura20260107 <y.miura@infomart.co.jp>
2026-01-22 09:55:53 +0900

    create project

---
+++ src/test/java/in/tech_camp/chat_app/ChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.tech_camp.chat_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
