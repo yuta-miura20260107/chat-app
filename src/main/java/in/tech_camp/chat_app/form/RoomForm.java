b31a6b93ccfc38fefaa2e3ebd85c69b7fba20765
yuta-miura20260107 <y.miura@infomart.co.jp>
2026-01-27 09:35:35 +0900

    ittan

--- src/main/java/in/tech_camp/chat_app/form/RoomForm.java
+++ src/main/java/in/tech_camp/chat_app/form/RoomForm.java
@@ -2,10 +2,14 @@ package in.tech_camp.chat_app.form;
 
 import java.util.List;
 
+import in.tech_camp.chat_app.validation.ValidationPriority1;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class RoomForm {
+  @NotBlank(message = "Room Name can't be blank",groups = ValidationPriority1.class)
   private String name;
+
   private List<Integer> memberIds;
 }
